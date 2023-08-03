aba3908606a0fa590a54f128db5fb225b5a97b7e
Дмитрий Варганов <varganovda@gmail.com>
2023-08-03 22:24:25 +0300

    feat(code): added a customers reducer

--- src/redux/reducers/customerReducer.js
+++ src/redux/reducers/customerReducer.js
@@ -4,7 +4,10 @@ const initialState = {
 
 export const customerReducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case 'ADD_CUSTOMER':
+      return {...state, customers: [...state.customers, action.payload]};
+    case 'REMOVE_CUSTOMER':
+      return {...state, customers: state.customers.filter((customer) => customer.id !== action.payload)};
     default:
       return state;
   }
