df4d48967bd639d84ec9601a79231352cbff1291
Дмитрий Варганов <varganovda@gmail.com>
2023-08-03 19:55:39 +0300

    feat(ui): added buttons to change the cash state

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash.cash);
-  console.log(cash);
+
+  const addCash = () => {
+    dispatch({type: 'ADD_CASH', payload: 5});
+  };
+
+  const getCash = () => {
+    dispatch({type: 'GET_CASH', payload: 5});
+  };
 
   return (
-    <div className="App">
-      cash
+    <div className='App'>
+      <p style={{fontSize: 24}}>{cash}</p>
+      <div style={{display: 'flex', width: 'fit-content', margin: '0 auto'}}>
+        <button onClick={() => addCash()}>Пополнить</button>
+        <button onClick={() => getCash()}>Снять</button>
+      </div>
     </div>
   );
 }
