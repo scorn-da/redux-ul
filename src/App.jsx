cb689e30a7560849ad44b2cc7c2b7cac89fe4fd6
Дмитрий Варганов <varganovda@gmail.com>
2023-08-03 19:25:27 +0300

    feat(code): connected the store in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash.cash);
+  console.log(cash);
+
   return (
     <div className="App">
+      cash
     </div>
   );
 }
