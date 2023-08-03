8a9bcc69801e2cf1e239645ceb0d59c94fe73b2b
Дмитрий Варганов <varganovda@gmail.com>
2023-08-03 22:24:51 +0300

    feat(ui): added a possibility to add and delete customers

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
     dispatch({type: 'ADD_CASH', payload: cash});
@@ -13,13 +14,37 @@ function App() {
     dispatch({type: 'GET_CASH', payload: cash});
   };
 
+  const addCustomer = (name) => {
+    const customer = {
+      id: Date.now(),
+      name,
+    };
+    dispatch({type: 'ADD_CUSTOMER', payload: customer});
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch({type: 'REMOVE_CUSTOMER', payload: customer});
+  };
+
   return (
     <div className='App'>
       <p style={{fontSize: 24}}>{cash}</p>
       <div style={{display: 'flex', width: 'fit-content', margin: '0 auto'}}>
         <button onClick={() => addCash(Number(prompt('Введите сумму')))}>Пополнить</button>
         <button onClick={() => getCash(Number(prompt('Введите сумму')))}>Снять</button>
+        <button onClick={() => addCustomer(prompt('Введите имя клиента'))}>Добавить клиента</button>
       </div>
+      {
+        customers.length > 0 ?
+            <ul>
+              {customers.map((customer) => {
+                return (<li style={{width: 'fit-content', margin: '4px auto', padding: 8, fontSize: 18, border: '1px solid black', listStyle: 'none'}} key={customer.id} onClick={() => removeCustomer(customer.id)}>{customer.name}</li>
+                )}
+              )}
+            </ul> :
+            <p style={{fontSize: 18, marginTop: 12}}>Клиенты отсутствуют</p>
+
+      }
     </div>
   );
 }
