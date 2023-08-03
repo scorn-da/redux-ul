da27f53a5efe54188a6770ce92a2b5a051b0a5de
Дмитрий Варганов <varganovda@gmail.com>
2023-08-03 20:09:24 +0300

    feat(code): added a customers reducer

---
+++ src/redux/reducers/customerReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  customers: [],
+};
+
+export const customerReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { cashReducer } from "src/redux/reducers/cashReducer";
+import { customerReducer } from "src/redux/reducers/customerReducer";
 
 export const rootReducer = combineReducers({
   cash: cashReducer,
+  customers: customerReducer,
 });
